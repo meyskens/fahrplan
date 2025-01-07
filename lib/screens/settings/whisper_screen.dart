b38a11b6767d307af28ea71c8bedc64dab4e642a
Maartje Eyskens <maartje@eyskens.me>
2025-01-07 19:58:53 +0000

    Add settings for remote whisper

--- lib/screens/settings/whisper_screen.dart
+++ lib/screens/settings/whisper_screen.dart
@@ -11,6 +11,10 @@ class WhisperSettingsPage extends StatefulWidget {
 class WhisperSettingsPageState extends State<WhisperSettingsPage> {
   final List<String> _models = FahrplanWhisperModel.models;
   String? _selectedModel;
+  String? _selectedMode;
+
+  final _apiUrlController = TextEditingController();
+  final _apiKeyController = TextEditingController();
 
   @override
   void initState() {
@@ -22,12 +26,23 @@ class WhisperSettingsPageState extends State<WhisperSettingsPage> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       _selectedModel = prefs.getString('whisper_model') ?? 'base';
+      _selectedMode = prefs.getString('whisper_mode') ?? 'local';
     });
   }
 
   Future<void> _saveSelectedModel(String model) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString('whisper_model', model);
+  }
+
+  Future<void> _saveSelectedMode(String mode) async {
+    setState(() {
+      _selectedMode = mode;
+    });
+    if (mode == "local") {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('whisper_mode', mode);
+    }
   }
 
   Future<void> _downloadModel() async {
@@ -52,41 +67,104 @@ class WhisperSettingsPageState extends State<WhisperSettingsPage> {
     );
   }
 
+  Future<void> _saveRemote() async {
+    // Implement the logic to download the model here
+    try {
+      if (_apiUrlController.text.isEmpty) {
+        throw Exception("API URL is required");
+      }
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('whisper_mode', _selectedMode!);
+      await prefs.setString('whisper_api_url', _apiUrlController.text);
+      await prefs.setString('whisper_api_key', _apiKeyController.text);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Whisper configuration saved!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error ${e.toString()}')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final localOpts = [
+      Text('Select Model:', style: TextStyle(fontSize: 18)),
+      SizedBox(height: 10),
+      DropdownButton<String>(
+        value: _selectedModel,
+        isExpanded: true,
+        onChanged: (String? newValue) {
+          setState(() {
+            _selectedModel = newValue;
+          });
+          _saveSelectedModel(newValue!);
+        },
+        items: _models.map<DropdownMenuItem<String>>((String model) {
+          return DropdownMenuItem<String>(
+            value: model,
+            child: Text(model),
+          );
+        }).toList(),
+      ),
+      SizedBox(height: 20),
+      ElevatedButton(
+        onPressed: _downloadModel,
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 36), // Expand full width
+        ),
+        child: const Text('Download Model'),
+      ),
+    ];
+    final remoteOpts = [
+      Text('Whisper server details:', style: TextStyle(fontSize: 18)),
+      SizedBox(height: 10),
+      TextField(
+        decoration: InputDecoration(labelText: 'API URL'),
+        controller: _apiUrlController,
+      ),
+      SizedBox(height: 10),
+      TextField(
+        decoration: InputDecoration(labelText: 'API Key'),
+        controller: _apiKeyController,
+      ),
+      SizedBox(height: 20),
+      ElevatedButton(
+        onPressed: _saveRemote,
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 36), // Expand full width
+        ),
+        child: Text('Save'),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('Whisper Settings'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Select Model:', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 10),
-            DropdownButton<String>(
-              value: _selectedModel,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedModel = newValue;
-                });
-                _saveSelectedModel(newValue!);
-              },
-              items: _models.map<DropdownMenuItem<String>>((String model) {
-                return DropdownMenuItem<String>(
-                  value: model,
-                  child: Text(model),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _downloadModel,
-              child: Text('Download Model'),
-            ),
-          ],
-        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text('Select Mode:', style: TextStyle(fontSize: 18)),
+          SizedBox(height: 10),
+          DropdownButton(
+              value: _selectedMode,
+              onChanged: (String? newValue) => _saveSelectedMode(newValue!),
+              isExpanded: true,
+              items: [
+                DropdownMenuItem(
+                  value: "local",
+                  child: Text("Local"),
+                ),
+                DropdownMenuItem(
+                  value: "remote",
+                  child: Text("Remote"),
+                )
+              ]),
+          ...(_selectedMode == "local" ? localOpts : remoteOpts),
+        ]),
       ),
     );
   }
