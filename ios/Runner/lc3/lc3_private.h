11b63e0ac5a4c5402e44c509d8f06df9a3f92669
Maartje Eyskens <maartje@eyskens.me>
2026-06-11 09:01:04 +0200

    fix headers in lc3 priv

--- ios/Runner/lc3/lc3_private.h
+++ ios/Runner/lc3/lc3_private.h
@@ -19,12 +19,12 @@
 #ifndef __LC3_PRIVATE_H
 #define __LC3_PRIVATE_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <stdint.h>
-#include <stdbool.h>
 
 
 /**
